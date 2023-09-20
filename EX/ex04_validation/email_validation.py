83a404adbbc850cafa08ed5fa9c31d31665f4583
Indrek Mäeots <inmaeo@taltech.ee>
2023-09-20 16:25:31 +0300

    ex04_validation completed email_validation functions and received correct tests.

--- EX/ex04_validation/email_validation.py
+++ EX/ex04_validation/email_validation.py
@@ -1,6 +1,68 @@
 """Email validation."""
 
+
 # Write your functions here
+def has_at_symbol(email: str):
+    for i in email:
+        if i == "@":
+            return True
+
+    return False
+
+
+def is_valid_username(email: str):
+    email_parts = email.split("@")
+    if len(email_parts) > 2:
+        return False
+    else:
+        for i in email_parts[0]:
+            if not i.isalnum():
+                if i == ".":
+                    continue
+                else:
+                    return False
+
+        return True
+
+
+def find_domain(email: str):
+    return email.split("@")[-1]
+
+
+def is_valid_domain(email: str):
+    domain = find_domain(email)
+    dot_count = 0
+    count = 0
+    for i in domain:
+        count += 1
+        if not i.isalpha():
+            if i == "." and dot_count == 0:
+                dot_count = 1
+                if count < 3 or count > 10:
+                    return False
+                else:
+                    count = 0
+            else:
+                return False
+    if count < 2 or count > 5:
+        return False
+    elif dot_count == 1:
+        return True
+    else:
+        return False
+
+
+def is_valid_email_address(email: str):
+    return has_at_symbol(email) and is_valid_username(email) \
+        and is_valid_domain(email)
+
+
+def create_email_address(domain: str, username: str):
+    email = f"{username}@{domain}"
+    if is_valid_email_address(email):
+        return email
+    else:
+        return "Cannot create a valid email address using the given parameters!"
 
 
 if __name__ == '__main__':
@@ -36,4 +98,3 @@ if __name__ == '__main__':
     print(create_email_address("jaani.org", "lennakuurma"))  # -> lennakuurma@jaani.org
     print(create_email_address("koobas.com",
                                "karu&pojad"))  # -> Cannot create a valid email address using the given parameters!
-
