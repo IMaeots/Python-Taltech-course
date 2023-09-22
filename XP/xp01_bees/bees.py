abd6dfc88959474a656091600adcb0c442c280b3
Indrek Mäeots <inmaeo@taltech.ee>
2023-09-22 22:46:33 +0300

    first complete

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -18,7 +18,8 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpaddle_data:
     if honeycomb_width <= 0 or len(honeyhopper_data.split(',')) < 4 or len(pollenpaddle_data.split(',')) < 4:
         raise ValueError("Insufficient data for sequence identification")
 
-    """Defining helper functions."""
+    'Defining helper functions.'
+
     def honeyhopper_new_position(overall_position: int) -> int:
         current_hex = overall_position % TOTAL_HEXES
         if current_hex == 0:
@@ -33,15 +34,14 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpaddle_data:
             return current_hex
 
     def calculate_complete_bee_data(bee_data: str) -> list[int]:
-        # Recycle out the numbers
+        # Recycle out the numbers.
         data_list = [int(num) for num in bee_data.split(',')]
 
-        # Find the differences
+        # Find the differences.
         differences = [data_list[i + 1] - data_list[i] for i in range(3)]
 
         # Check for constant:
         if all(diff == differences[0] for diff in differences):
-            # Constant: (hüpete vahel on konstantne vahe).
             new_data_list = data_list[:4]
             i = 3
             while i < 100000:
@@ -50,24 +50,52 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpaddle_data:
 
             return new_data_list
 
-        # Check for arithmetic:
-        # Arithmetic increase: (hüpete vahel on ühtlaselt suurenev vahe.) (1,2,4,7) - kordaja suureneb mingi arv võrra
-        # TODO
+        # Check for arithmetic increase:
+        if differences[2] - differences[1] == differences[0]:
+            increment = differences[1] - differences[0]
+            new_data_list = data_list[:4]
+            i = 3
+            while i < 100000:
+                last_step = new_data_list[i] - new_data_list[i - 1]
+                new_data_list.append(new_data_list[i] + last_step + increment)
+                i += 1
 
-        # Check for geometrical increase and geometrical step distance:
-        # Geometric progression (positsioonid moodustavad gemoeetrilise jada.) (1,2,4,8) - sama kordaja
-        #ratios = [data_list[i + 1] / data_list[i] for i in range(3)]
-        #if all(ratio == ratios[0] for ratio in ratios):
-        #    step = int(data_list[1] / data_list[2])
-        #    return
+            return new_data_list
+
+        # Find the ratios.
+        ratios = [data_list[i + 1] / data_list[i] for i in range(3)]
+
+        # Check for geometrical increase:
+        if all(ratio == ratios[0] for ratio in ratios):
+            multiplier = ratios[0]
+            new_data_list = data_list[:4]
+            i = 3
+            while i < 100000 and i < len(new_data_list):
+                new_value = new_data_list[i] * multiplier
+                if abs(new_value) != float("inf"):
+                    new_data_list.append(int(new_value))
+                i += 1
+
+            return new_data_list
 
         # Check for geometric step:
-        # (hüpete vahe suureneb geomeetrilise jada võrra.) (1,3,7,15) - kordaja suureneb mingi arv korda
-        # TODO
+        if differences[2] - differences[1] == differences[0]:
+            multiplier = ratios[0]
+            new_data_list = data_list[:4]
+            i = 3
+            while i < 100000:
+                last_step = new_data_list[i] - new_data_list[i - 1]
+                new_value = new_data_list[i] + last_step * multiplier
+                if abs(new_value) != float("inf"):
+                    new_data_list.append(int(new_value))
+                i += 1
 
-        return data_list
+            return new_data_list
 
-    """Function that simulates bees movement."""
+        raise ValueError("Insufficient data for sequence identification")
+
+    'Function that simulates bees movement.'
+
     def simulation(honeyhopper, pollenpaddle) -> bool:
         for pos1, pos2 in zip(honeyhopper, pollenpaddle):
             if honeyhopper_new_position(pos1) == pollenpaddle_new_position(pos2):
@@ -77,7 +105,7 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpaddle_data:
 
         return False
 
-    # Calling the simulation function
+    # Calling the simulation function.
     return simulation(calculate_complete_bee_data(honeyhopper_data), calculate_complete_bee_data(pollenpaddle_data))
 
 
