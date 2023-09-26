04641647ca07e773d8dab7887ebc907a4000aa4b
Indrek Mäeots <inmaeo@taltech.ee>
2023-09-26 09:56:32 +0300

    testing

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -15,6 +15,8 @@ def check_input(honeyhopper_data, pollenpaddle_data, honeycomb_width):
     if honeycomb_width <= 0 or len(honeyhopper_numeric_data) < 4 or len(pollenpaddle_numeric_data) < 4:
         error()
 
+    raise ValueError(f"{honeyhopper_data},{pollenpaddle_data},{honeycomb_width}")
+
 
 def honeyhopper_new_position(overall_position: int, TOTAL_HEXES) -> int:
     """Generate honeyhopper position."""
