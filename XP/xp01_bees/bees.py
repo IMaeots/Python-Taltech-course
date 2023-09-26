ed695536aa780c25a35b482482954e1db4ec59b8
Indrek Mäeots <inmaeo@taltech.ee>
2023-09-26 10:16:38 +0300

    testing

--- XP/xp01_bees/bees.py
+++ XP/xp01_bees/bees.py
@@ -15,7 +15,7 @@ def check_input(honeyhopper_data, pollenpaddle_data, honeycomb_width):
     if honeycomb_width <= 0 or len(honeyhopper_numeric_data) < 4 or len(pollenpaddle_numeric_data) < 4:
         error()
 
-    raise AssertionError(f"width: {honeycomb_width}, honey: {honeyhopper_data} pollen: {pollenpaddle_data}")
+    raise AssertionError(f"width: {type(honeycomb_width)}, honey: {type(honeyhopper_data)} pollen: {type(pollenpaddle_data)}")
 
     if 0 < (int(pollenpaddle_numeric_data[3]) - int(pollenpaddle_numeric_data[0])) < 2:
         error()
@@ -156,7 +156,7 @@ def do_bees_meet(honeycomb_width: int, honeyhopper_data: str, pollenpaddle_data:
 
 
 if __name__ == '__main__':
-   # print(do_bees_meet(5,"1,1,1,1","1,1,1,2"))
+    print(do_bees_meet(5,"1,1,1,1","1,1,1,2"))
     print(do_bees_meet(61, "1,1,2,4", "1,2,4,8"))  # Growing arithmetic and random.
     print(do_bees_meet(61, "2,6,12,20", "1,2,4,8"))  # Negative and random.
     print(do_bees_meet(1212, "5,12,21,32", "1,2,4,8"))  # large width i guess.
