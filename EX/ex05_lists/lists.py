3c335a1b141b3edabdd16503032694d460b87c7e
Indrek Mäeots <inmaeo@taltech.ee>
2023-09-28 22:13:34 +0300

    fixed searches

--- EX/ex05_lists/lists.py
+++ EX/ex05_lists/lists.py
@@ -68,28 +68,48 @@ def phone_models(all_phones: str) -> list:
 def search_by_brand(all_phones: str, search: str) -> list:
     """Return list of results based on the searched brad."""
     brands = phone_brands(all_phones)
+    searched_brand = ""
     for i in brands:
         if search.lower() in i.lower():
-            return i
+            searched_brand = i
 
-    return []
+    if searched_brand != "":
+        result_list = []
+        for phone in list_of_phones(all_phones):
+            if searched_brand in phone:
+                result_list.append(phone)
+
+        return result_list
+    else:
+        return []
 
 
 def search_by_model(all_phones: str, search: str) -> list:
     """Return list of results based on the searched model."""
     models = phone_models(all_phones)
     case_insensitive_search = search.lower()
+    searched_model = ""
     for model in models:
         for list_model in model:
             for i in range(len(list_model)):
                 if case_insensitive_search == list_model[i].lower():
-                    return list_model
+                    searched_model = model
 
-    return []
+    if searched_model != "":
+        result_list = []
+        for phone in list_of_phones(all_phones):
+            if searched_model in phone:
+                result_list.append(phone)
+
+        return result_list
+    else:
+        return []
 
 
 print(list_of_phones("Google Pixel,Honor Magic5,Google Pixel"))  # ["Google Pixel', 'Honor Magic5', 'Google Pixel"]
-print(phone_brands("Google Pixel,Honor Magic5,Google Pix,Honor Magic6,IPhone 12,Samsung S10,Honor Magic,IPhone 11"))  # ['Google', 'Honor', 'IPhone', 'Samsung']
+print(phone_brands(
+    "Google Pixel,Honor Magic5,Google Pix,Honor Magic6,IPhone 12,Samsung S10,Honor Magic,IPhone 11"))  # ['Google', 'Honor', 'IPhone', 'Samsung']
 print(phone_brands("Google Pixel,Google Pixel,Google Pixel,Google Pixel"))  # ['Google']
 print(phone_brands(""))  # []
 print(phone_models("IPhone 14,Google Pixel,Honor Magic5,IPhone 14 Pro"))  # ['14', 'Pixel', 'Magic5']
+print(search_by_brand("Google Pixel, Iphone 11 Pro", 'Google'))  # ['Google Pixel']
