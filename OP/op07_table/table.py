088e521e35d05b4b6f069cf9fcb4a5779c5ad7d6
Indrek Mäeots <inmaeo@taltech.ee>
2023-10-22 12:37:46 +0300

    Revert "added sorting"
    This reverts commit e3ece41537290eb787e7de30d4e12d793f9d05a5.

--- OP/op07_table/table.py
+++ OP/op07_table/table.py
@@ -93,7 +93,7 @@ def get_times(text: str) -> list[tuple[int, int, int]]:
         else:
             times.append((hour, minute, offset))
 
-    return sorted(times)
+    return times
 
 
 def get_usernames(text: str) -> list[str]:
@@ -104,7 +104,7 @@ def get_usernames(text: str) -> list[str]:
     if matches is None:
         return []
 
-    return sorted(matches)
+    return matches
 
 
 def get_errors(text: str) -> list[int]:
@@ -115,7 +115,7 @@ def get_errors(text: str) -> list[int]:
     if matches is None:
         return []
 
-    return [int(match) for match in sorted(matches)]
+    return [int(match) for match in matches]
 
 
 def get_addresses(text: str) -> list[str]:
@@ -126,7 +126,7 @@ def get_addresses(text: str) -> list[str]:
     if matches is None:
         return []
 
-    return sorted(matches)
+    return matches
 
 
 def get_endpoints(text: str) -> list[str]:
@@ -137,7 +137,7 @@ def get_endpoints(text: str) -> list[str]:
     if matches is None:
         return []
 
-    return sorted(matches)
+    return matches
 
 
 if __name__ == '__main__':
