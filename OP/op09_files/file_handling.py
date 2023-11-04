6d9eb1f4302397f8dd20f98051a1d66858f46dda
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-04 13:38:21 +0200

    .

--- OP/op09_files/file_handling.py
+++ OP/op09_files/file_handling.py
@@ -9,16 +9,11 @@ def cast_value(value, data_type):
     """Cast value to the appropriate data type based on the column's data_type."""
     if value == "-":
         return None
-
-    try:
-        if data_type == int:
-            return int(value)
-        elif data_type == datetime:
-            return datetime.strptime(value, "%d.%m.%Y").date()
-        else:
-            return str(value)
-    except (ValueError, TypeError):
-        return str(value)
+    if data_type == int:
+        return int(value)
+    if data_type == datetime:
+        return datetime.strptime(value, "%d.%m.%Y").date()
+    return str(value)
 
 
 def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list[dict]:
