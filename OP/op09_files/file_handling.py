b4aeebf05d3c7766cd892bc59cde41ed53411988
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-09 13:09:18 +0200

    changed data type inside assignment.

--- OP/op09_files/file_handling.py
+++ OP/op09_files/file_handling.py
@@ -128,11 +128,13 @@ def read_csv_file_into_list_of_dicts_using_datatypes(filename: str) -> list[dict
                         try:
                             processed_row[key] = int(value)
                         except ValueError:
+                            data_types[key] = str
                             processed_row[key] = str(value)
                     elif data_types[key] == datetime:
                         try:
                             processed_row[key] = datetime.strptime(value, "%d.%m.%Y").date()
                         except ValueError:
+                            data_types[key] = str
                             processed_row[key] = str(value)
                     else:
                         processed_row[key] = str(value)
