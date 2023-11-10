285eb40d5599c82ba3127ed01ad0b083e89cfd97
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-10 10:29:49 +0200

    added function headers.

---
+++ WAT/wat/wat.py
@@ -0,0 +1,41 @@
+"""Wat."""
+
+
+def function_a():
+    return 1
+
+
+def function_b():
+    return 1
+
+
+def function_c():
+    return 1
+
+
+def function_d():
+    return 1
+
+
+def function_e():
+    return 1
+
+
+def function_f():
+    return 1
+
+
+def function_g():
+    return 1
+
+
+def function_h():
+    return 1
+
+
+def function_i():
+    return 1
+
+
+def function_j():
+    return 1
