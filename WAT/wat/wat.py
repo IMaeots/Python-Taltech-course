d865c548529f483a1d4ce7406dc1d54675e3236e
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-10 10:33:11 +0200

    made function headers more correct.

--- WAT/wat/wat.py
+++ WAT/wat/wat.py
@@ -1,41 +1,51 @@
 """Wat."""
 
 
-def function_a():
-    return 1
+def function_a(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_b():
-    return 1
+def function_b(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_c():
-    return 1
+def function_c(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_d():
-    return 1
+def function_d(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_e():
-    return 1
+def function_e(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_f():
-    return 1
+def function_f(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_g():
-    return 1
+def function_g(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_h():
-    return 1
+def function_h(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_i():
-    return 1
+def function_i(x):
+    """Do what you got to do."""
+    return x
 
 
-def function_j():
-    return 1
+def function_j(x):
+    """Do what you got to do."""
+    return x
