0d6b26a27fcf3df9652ecec1ad0aafdaa73e6ded
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 18:23:31 +0200

    made the complete test layout and first 6 tests.

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -4,22 +4,235 @@ from solution import lottery
 from solution import fruit_order
 
 
-def test_students_study_during_day():
-    """The one with the coffee at noon."""
-    assert students_study(12, True) is True
+"""Students study function tests."""
 
 
-def test_lottery_jackpot():
-    """Winning jackpot in the lottery."""
-    assert lottery(5, 5, 5) == 10
+def test_student_study__night_coffee_true():
+    """Student study test."""
+    for num in range(1, 5):
+        assert students_study(num, True) is False
 
 
-def test_lottery_all_same_numbers():
-    """Same numbers."""
-    for num in range(1, 9):
-        assert lottery(num, num, num) == 5
+def test_student_study__night_coffee_false():
+    """Student study test."""
+    for num in range(1, 5):
+        assert students_study(num, False) is False
 
 
-def test_correct_fruit_order():
-    """Correct fruit order."""
-    assert fruit_order(4, 1, 9) is True
+def test_student_study__day_coffee_true():
+    """Student study test."""
+    for num in range(5, 18):
+        assert students_study(num, True) is True
+
+
+def test_student_study__day_coffee_false():
+    """Student study test."""
+    for num in range(5, 18):
+        assert students_study(num, False) is False
+
+
+def test_student_study__evening_coffee_true():
+    """Student study test."""
+    for num in range(18, 25):
+        assert students_study(num, True) is True
+
+
+def test_student_study__evening_coffee_false():
+    """Student study test."""
+    for num in range(18, 25):
+        assert students_study(num, False) is True
+
+
+def test_student_study__evening_edge_case_coffee_true():
+    """Student study test."""
+    pass
+
+
+def test_student_study__evening_edge_case_coffee_false():
+    """Student study test."""
+    pass
+
+
+def test_student_study__night_edge_case_coffee_true():
+    """Student study test."""
+    pass
+
+
+def test_student_study__night_edge_case_coffee_false():
+    """Student study test."""
+    pass
+
+
+def test_student_study__day_edge_case_coffee_true():
+    """Student study test."""
+    pass
+
+
+def test_student_study__day_edge_case_coffee_false():
+    """Student study test."""
+    pass
+
+
+"""Lottery function tests."""
+
+
+def test_lottery__all_fives():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__all_same_positive():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__all_same_negative():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__all_same_zero():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__a_b_same_c_diff():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__a_c_same_b_diff():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__b_c_same_a_diff():
+    """Lottery function test."""
+    pass
+
+
+def test_lottery__all_diff():
+    """Lottery function test."""
+    pass
+
+
+"""Fruit order function tests."""
+
+
+def test_fruit_order__all_zero():
+    """Fruit order function test."""
+    pass
+
+def test_fruit_order__zero_amount_zero_small():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__zero_amount_zero_big():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__zero_amount_others_not_zero():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_big_exact_match():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_big_not_enough_but_multiple_of_5():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_big_not_enough():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_big_more_than_required_match():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_big_more_than_required_no_match():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_small_match_more_than_5_smalls():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_small_not_enough_more_than_5_smalls():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_small_exact_match():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_small_not_enough():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__only_small_more_than_required():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__match_with_more_than_5_smalls():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__all_positive_exact_match():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__use_all_smalls_some_bigs():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__use_some_smalls_all_bigs():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__use_some_smalls_some_bigs():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__not_enough():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__enough_bigs_not_enough_smalls():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__not_enough_with_more_than_5_smalls():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__enough_bigs_not_enough_smalls_large_numbers():
+    """Fruit order function test."""
+    pass
+
+
+def test_fruit_order__match_large_numbers():
+    """Fruit order function test."""
+    pass
