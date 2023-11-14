0ba89e33ec66cdc9b35b58bf446e5838045d2f4c
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 19:36:15 +0200

    reverted back a test

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -190,11 +190,7 @@ def test_fruit_order__use_some_smalls_some_bigs():
 
 def test_fruit_order__not_enough():
     """Fruit order function test."""
-    for num1 in range(1, 10):
-        for num2 in range(1, 10):
-            for num3 in range(1, 50):
-                if num3 < (num2 * 5 + num1):
-                    assert fruit_order(num1, num2, num3) == -1
+    pass
 
 
 def test_fruit_order__enough_bigs_not_enough_smalls():
