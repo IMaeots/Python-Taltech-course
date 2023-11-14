c891bed8edf56e1a0392ff2bbde582aa5c428dbb
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 19:22:41 +0200

    removed to check if error

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -118,14 +118,6 @@ def test_fruit_order__only_big_exact_match():
         assert fruit_order(0, num1, (num1 * 5)) == 0
 
 
-def test_fruit_order__only_big_not_enough_but_multiple_of_5():
-    """Fruit order function test."""
-    for num1 in range(1, 10):
-        for num2 in range(5, 51, 5):
-            if num1 > num2:
-                assert fruit_order(0, num1, num2) == -1
-
-
 def test_fruit_order__only_big_not_enough():
     """Fruit order function test."""
     pass
