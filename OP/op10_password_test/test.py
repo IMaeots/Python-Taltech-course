9fd7b95320ac0ddf16d1cf03d488bd8e6106878b
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 19:52:27 +0200

    finished is_correct_length tests

--- OP/op10_password_test/test.py
+++ OP/op10_password_test/test.py
@@ -9,7 +9,7 @@ def test_is_correct_length__empty():
     assert password.is_correct_length("") is False
 
 
-def test__is_correct_length__too_short():
+def test_is_correct_length__too_short():
     """Test whether password of length up to 7 is not correct."""
     assert password.is_correct_length("p") is False
     assert password.is_correct_length("pa") is False
@@ -20,7 +20,13 @@ def test__is_correct_length__too_short():
     assert password.is_correct_length("passwor") is False
 
 
-def test__is_correct_length__too_long():
+def test_is_correct_length__too_long():
     """Test whether password of length > 64 is incorrect."""
     assert password.is_correct_length("password" * 8 + 'a') is False
     assert password.is_correct_length("pass" * 18) is False
+
+
+def test_is_correct_length__edge_cases():
+    """Test edge cases."""
+    assert password.is_correct_length("password" * 8) is True
+    assert password.is_correct_length("password") is True
