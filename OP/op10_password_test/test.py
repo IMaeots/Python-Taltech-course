c6fd481bd7ab3743735a018cc8785559b7f5434f
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 19:42:31 +0200

    base

---
+++ OP/op10_password_test/test.py
@@ -0,0 +1,12 @@
+"""Password validation tests."""
+# from EX.ex04_validation import password
+
+
+def test__is_correct_length__too_short():
+    """Test whether password of length 7 is not correct."""
+    assert password.is_correct_length("passwor") is False
+
+
+def test__is_correct_length__too_long():
+    """Test whether password of length > 64 is incorrect."""
+    assert password.is_correct_length("pass" * 18) is False
