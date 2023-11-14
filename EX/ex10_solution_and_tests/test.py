4ea3b29b0bcebafb157090749c46c23b9a1966e7
Indrek Mäeots <inmaeo@taltech.ee>
2023-11-14 17:58:22 +0200

    base testcases

--- EX/ex10_solution_and_tests/test.py
+++ EX/ex10_solution_and_tests/test.py
@@ -3,40 +3,21 @@ from solution import *
 
 
 def test_students_study_during_day():
-    """
-    The one with the coffee at noon.
-
-    During the day, students study when there is coffee.
-    This case represents the time period of a day and coffee is present.
-    Expected result: True.
-    """
+    """The one with the coffee at noon."""
     assert students_study(12, True) is True
 
 
 def test_lottery_jackpot():
-    """
-    Winning jackpot in the lottery.
-
-    All numbers must be 5.
-    Expected result: 10.
-    """
+    """Winning jackpot in the lottery."""
     assert lottery(5, 5, 5) == 10
 
 
 def test_lottery_all_same_numbers():
-    """
-    Same numbers.
-
-    Expected result: 5.
-    """
-    for num in range(1,9):
+    """Same numbers."""
+    for num in range(1, 9):
         assert lottery(num, num, num) == 5
 
 
 def test_correct_fruit_order():
-    """
-    Fruit order.
-
-    Expected result: True.
-    """
+    """Correct fruit order"""
     assert fruit_order(4, 1, 9) is True
