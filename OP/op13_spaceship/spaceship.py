56970f432756eadf15d8e3e66c814e736d631222
Indrek Mäeots <inmaeo@taltech.ee>
2023-12-05 23:13:16 +0200

    initial push - game ending is unfinished other almost ok

---
+++ OP/op13_spaceship/spaceship.py
@@ -0,0 +1,270 @@
+"""Spaceship."""
+
+
+class Crewmate:
+    """Crewmate class."""
+
+    def __init__(self, color: str, role: str, tasks: int = 10):
+        """Construct the class."""
+        self.color = color.capitalize()
+
+        possible_roles = ["CREWMATE", "SHERIFF", "GUARDIAN ANGEL", "ALTRUIST"]
+        if role.upper() in possible_roles:
+            self.role = role.title()
+        else:
+            self.role = possible_roles[0].title()
+
+        self.tasks = tasks
+        self.protected = False
+
+    def __repr__(self):
+        """Magic method repr for string representation."""
+        return f"{self.color}, role: {self.role}, tasks left: {self.tasks}."
+
+    def complete_task(self):
+        """Subtract task from tasks list."""
+        if self.tasks > 1:
+            self.tasks -= 1
+        else:
+            return
+
+
+class Impostor:
+    """Impostor class."""
+
+    def __init__(self, color: str):
+        """Construct the class."""
+        self.color = color.title()
+        self.kills = 0
+
+    def __repr__(self):
+        """Magic method repr for string representation."""
+        return f"Impostor {self.color}, kills: {self.kills}."
+
+
+class Spaceship:
+    """Spaceship class."""
+
+    def __init__(self):
+        """Construct the class."""
+        self.crewmate_list = []
+        self.impostor_list = []
+        self.dead_players = []
+
+    def get_crewmate_list(self):
+        """Get list of crewmates."""
+        return self.crewmate_list
+
+    def add_crewmate(self, crewmate: Crewmate):
+        """Add a crewmate."""
+        if isinstance(crewmate, Crewmate) and self.check_if_new_player(crewmate):
+            self.crewmate_list.append(crewmate)
+
+    def get_impostor_list(self):
+        """Get list of impostors."""
+        return self.impostor_list
+
+    def add_impostor(self, impostor: Impostor):
+        """Add an impostor."""
+        if isinstance(impostor, Impostor) and len(self.impostor_list) < 3 and self.check_if_new_player(impostor):
+            self.impostor_list.append(impostor)
+
+    def get_dead_players(self):
+        """Get list of dead players."""
+        return self.dead_players
+
+    def check_if_new_player(self, player):
+        """Control if the body-color is not already in use."""
+        for crewmate in self.crewmate_list:
+            if player.color.title() == crewmate.color:
+                return False
+
+        for impostor in self.impostor_list:
+            if player.color.title() == impostor.color:
+                return False
+
+        for dead in self.dead_players:
+            if player.color.title() == dead.color:
+                return False
+
+        return True
+
+    def kill_impostor(self, sheriff: Crewmate, color: str):
+        """Murder an imposter if the chosen color is correct."""
+        if sheriff in self.crewmate_list and sheriff.role == "Sheriff":
+            person_to_remove = None
+            for person in self.impostor_list:
+                if person.color == color.title():
+                    person_to_remove = person
+
+            if person_to_remove:
+                self.impostor_list.remove(person_to_remove)
+                self.dead_players.append(person_to_remove)
+            else:
+                self.crewmate_list.remove(sheriff)
+                self.dead_players.append(sheriff)
+
+        return
+
+    def revive_crewmate(self, altruist: Crewmate, dead_crewmate: Crewmate):
+        """Help a friend in need."""
+        if altruist in self.crewmate_list and altruist.role == "Altruist" \
+                and dead_crewmate in self.dead_players:
+            self.dead_players.remove(dead_crewmate)
+            self.crewmate_list.append(dead_crewmate)
+            self.crewmate_list.remove(altruist)
+            self.dead_players.append(altruist)
+
+    def protect_crewmate(self, guardian_angel: Crewmate, crewmate_to_protect: Crewmate):
+        """Enable protection for crewmate."""
+        if sum(person.protected for person in self.crewmate_list if person.protected is True) < 1:
+            if isinstance(guardian_angel, Crewmate) and \
+                    guardian_angel.role == "Guardian Angel" and guardian_angel in self.dead_players:
+                if isinstance(crewmate_to_protect, Crewmate) and crewmate_to_protect in self.crewmate_list:
+                    crewmate_to_protect.protected = True
+
+    def kill_crewmate(self, impostor: Impostor, color: str):
+        """Simulate killing a crewmate."""
+        if impostor in self.impostor_list:
+            the_person = None
+            for person in self.crewmate_list:
+                if person.color == color.title() and person not in self.dead_players:
+                    the_person = person
+
+            if the_person:
+                if the_person.protected:
+                    the_person.protected = False
+                else:
+                    self.dead_players.append(the_person)
+                    self.crewmate_list.remove(the_person)
+                    impostor.kills += 1
+
+    def sort_crewmates_by_tasks(self):
+        """Sort crewmates by tasks."""
+        return sorted(self.crewmate_list, key=lambda x: x.tasks)
+
+    def sort_impostors_by_kills(self):
+        """Sort impostors by kills."""
+        return sorted(self.impostor_list, key=lambda x: -x.kills)
+
+    def get_regular_crewmates(self):
+        """Return a list of regular crewmates."""
+        regulars = []
+        for i in self.crewmate_list:
+            if i.role == "Crewmate":
+                regulars.append(i)
+
+        return regulars
+
+    def get_role_of_player(self, color: str):
+        """Return the role of player."""
+        for player in self.crewmate_list:
+            if player.color == color.title():
+                return player.role
+
+        for player in self.impostor_list:
+            if player.color == color.title():
+                return "Impostor"
+
+    def get_crewmate_with_most_tasks_done(self):
+        """Return crewmate with the most tasks done."""
+        the_one = self.crewmate_list[0]
+        for crewmate in self.crewmate_list:
+            if crewmate.tasks < the_one.tasks:
+                the_one = crewmate
+
+        return the_one
+
+    def get_impostor_with_most_kills(self):
+        """Return the impostor with most kills."""
+        the_one = self.impostor_list[0]
+        for imposter in self.impostor_list:
+            if imposter.kills > the_one.kills:
+                the_one = imposter
+
+        return the_one
+
+
+if __name__ == "__main__":
+    print("Spaceship.")
+
+    spaceship = Spaceship()
+    print(spaceship.get_dead_players())  # -> []
+    print()
+
+    print("Let's add some crewmates.")
+    red = Crewmate("Red", "Crewmate")
+    white = Crewmate("White", "Impostor")
+    yellow = Crewmate("Yellow", "Guardian Angel", tasks=5)
+    green = Crewmate("green", "Altruist")
+    blue = Crewmate("BLUE", "Sheriff", tasks=0)
+
+    print(red)  # -> Red, role: Crewmate, tasks left: 10.
+    print(white)  # -> White, role: Crewmate, tasks left: 10.
+    print(yellow)  # -> Yellow, role: Guardian Angel, tasks left: 5.
+    print(blue)  # -> Blue, role: Sheriff, tasks left: 0.
+    print()
+
+    print("Let's make Yellow complete a task.")
+    yellow.complete_task()
+    print(yellow)  # ->  Yellow, role: Guardian Angel, tasks left: 4.
+    print()
+
+    print("Adding crewmates to Spaceship:")
+    spaceship.add_crewmate(red)
+    spaceship.add_crewmate(white)
+    spaceship.add_crewmate(yellow)
+    spaceship.add_crewmate(green)
+    print(spaceship.get_crewmate_list())  # -> Red, White, Yellow and Green
+
+    spaceship.add_impostor(blue)  # Blue cannot be an Impostor.
+    print(spaceship.get_impostor_list())  # -> []
+    spaceship.add_crewmate(blue)
+    print()
+
+    print("Now let's add impostors.")
+    orange = Impostor("orANge")
+    black = Impostor("black")
+    purple = Impostor("Purple")
+    spaceship.add_impostor(orange)
+    spaceship.add_impostor(black)
+
+    spaceship.add_impostor(Impostor("Blue"))  # Blue player already exists in Spaceship.
+    spaceship.add_impostor(purple)
+    spaceship.add_impostor(Impostor("Pink"))  # No more than three impostors can be on Spaceship.
+    print(spaceship.get_impostor_list())  # -> Orange, Black and Purple
+    print()
+
+    print("The game has begun! Orange goes for the kill.")
+    spaceship.kill_crewmate(orange, "yellow")
+    print(orange)  # -> Impostor Orange, kills: 1.
+    spaceship.kill_crewmate(black, "purple")  # You can't kill another Impostor, silly!
+    print(spaceship.get_dead_players())  # -> Yellow
+    print()
+
+    spaceship.kill_impostor(blue, "black")
+    print(spaceship.get_impostor_list())
+    print()
+
+    print("Yellow is a Guardian angel, and can protect their allies when dead.")
+    spaceship.protect_crewmate(yellow, green)
+    print(green.protected)  # -> True
+    spaceship.kill_crewmate(orange, "green")
+    print(green in spaceship.dead_players)  # -> False
+    print(green.protected)  # -> False
+    print()
+
+    print("Green revives their ally.")
+    spaceship.kill_crewmate(purple, "RED")
+    spaceship.revive_crewmate(green, red)
+    print(red in spaceship.dead_players)  # -> False
+    print()
+
+    print("Let's check if the sorting and filtering works correctly.")
+
+    red.complete_task()
+    print(spaceship.get_role_of_player("Blue"))  # -> Sheriff
+    spaceship.kill_crewmate(purple, "blue")
+    print(spaceship.sort_crewmates_by_tasks())  # -> Red, White
+    print(spaceship.sort_impostors_by_kills())  # -> Purple, Orange, Black
+    print(spaceship.get_regular_crewmates())  # -> White, Red
---
+++ OP/op13_spaceship/spaceship_op.py
@@ -0,0 +1,116 @@
+"""Spaceship OP."""
+
+import spaceship
+
+
+class OPSpaceship(spaceship.Spaceship):
+    """OP version of spaceship."""
+    def __init__(self, difficulty: str):
+        """Initialize the class."""
+        super().__init__()
+        if difficulty.lower() in ["easy", "hard"]:
+            self.difficulty = difficulty.lower()
+        else:
+            self.difficulty = "hard"
+
+        self.ejected_players = []
+        self.meeting = False
+        self.votes = {}
+        self.game = False
+
+    def add_crewmate(self, crewmate: spaceship.Crewmate):
+        """Add crewmate."""
+        if self.game is False:
+            super().add_crewmate(crewmate)
+
+    def add_impostor(self, impostor: spaceship.Impostor):
+        """Add impostor."""
+        if self.game is False:
+            super().add_impostor(impostor)
+
+    def kill_impostor(self, sheriff: spaceship.Crewmate, color: str):
+        """Kill impostor."""
+        if self.game is True and self.meeting is False:
+            super().kill_impostor(sheriff, color)
+
+            game_over = self.check_if_game_over()
+
+    def kill_crewmate(self, impostor: spaceship.Impostor, color: str):
+        """Kill crewmate."""
+        if self.game is True and self.meeting is False:
+            super().kill_crewmate(impostor, color)
+
+            game_over = self.check_if_game_over()
+
+    def check_if_game_over(self):
+        """Check if the game is over."""
+        if self.game is True:
+            if len(self.impostor_list) < 1 or (len(self.crewmate_list) >= len(self.impostor_list) and self.impostor_list <= 3):
+                return True
+
+    def start_game(self):
+        """Start the game."""
+        if self.game is False:
+            if len(self.impostor_list) and len(self.crewmate_list) >= 2 and len(self.impostor_list) < len(self.crewmate_list):
+                self.game = True
+
+    def report_dead_body(self, reporting_player: spaceship.Crewmate | spaceship.Impostor, dead_body: spaceship.Crewmate | spaceship.Impostor):
+        """Report a dead body."""
+        if reporting_player not in self.dead_players or reporting_player not in self.ejected_players:
+            if dead_body in self.dead_players:
+                self.meeting = True
+
+    def cast_vote(self, player: spaceship.Crewmate | spaceship.Impostor, target_player_color: str):
+        """Cast vote in the meeting."""
+        if not self.votes[player.color] and self.game is True and self.meeting is True:
+            if target_player_color in [gamer.color for gamer in zip(self.crewmate_list, self.impostor_list)]:
+                self.votes[player.color] = target_player_color.title()
+
+    def end_meeting(self):
+        """End meeting."""
+        if self.game is True and self.meeting is True:
+            eliminated = max(self.votes)
+            if self.difficulty == "easy":
+                if eliminated in self.impostor_list:
+                    self.impostor_list.remove(eliminated)
+                    self.ejected_players.append(eliminated)
+                    impostors_left = len(self.impostor_list)
+                    if impostors_left > 1:
+                        return f"{eliminated.color} was an Impostor. {impostors_left} Impostors remains."
+                    else:
+                        return f"{eliminated.color} was an Impostor. {impostors_left} Impostor remain."
+                else:
+                    self.crewmate_list.remove(eliminated)
+                    self.ejected_players.append(eliminated)
+                    impostors_left = len(self.impostor_list)
+                    if impostors_left > 1:
+                        return f"{eliminated.color} was not an Impostor. {impostors_left} Impostors remains."
+                    else:
+                        return f"{eliminated.color} was not an Impostor. {impostors_left} Impostor remain."
+            else:
+                if eliminated in self.impostor_list:
+                    self.impostor_list.remove(eliminated)
+                    self.ejected_players.append(eliminated)
+                else:
+                    self.crewmate_list.remove(eliminated)
+                    self.ejected_players.append(eliminated)
+
+                self.check_if_game_over()
+
+                return f"{eliminated.color} was ejected."
+
+    def get_vote(self, color: str):
+        """Return who a player voted for."""
+        return self.votes[color.title()]
+
+    def get_ejected_players(self):
+        """Get ejected players."""
+        return self.ejected_players
+
+    def get_votes(self):
+        """Get votes."""
+        return self.votes
+
+    def is_meeting(self):
+        "Return if there is a meeting going on."
+        return self.meeting
