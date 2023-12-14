366b761f7ec34156c984a5f77b210518710b120d
Indrek Mäeots <inmaeo@taltech.ee>
2023-12-14 21:03:51 +0200

    for indent fix

--- AOC/aoc/day04a.py
+++ AOC/aoc/day04a.py
@@ -5,13 +5,13 @@ with open('day04_data.txt', 'r') as file:
 
 lines = text.split('\n')
 p1 = 0
-for i,line in enumerate(lines):
-  first, rest = line.split('|')
-  id_, card = first.split(':')
-  card_nums = [int(x) for x in card.split()]
-  rest_nums = [int(x) for x in rest.split()]
-  val = len(set(card_nums) & set(rest_nums))
-  if val > 0:
-    p1 += 2**(val-1)
+for i, line in enumerate(lines):
+    first, rest = line.split('|')
+    id_, card = first.split(':')
+    card_nums = [int(x) for x in card.split()]
+    rest_nums = [int(x) for x in rest.split()]
+    val = len(set(card_nums) & set(rest_nums))
+    if val > 0:
+        p1 += 2 ** (val - 1)
 
 print(p1)
