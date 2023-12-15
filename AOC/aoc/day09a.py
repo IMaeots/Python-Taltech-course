48303498b2751c7c982266bc1cb0cc6571b2c500
Indrek Mäeots <inmaeo@taltech.ee>
2023-12-15 14:07:30 +0200

    better day 9 solutions.

--- AOC/aoc/day09a.py
+++ AOC/aoc/day09a.py
@@ -3,31 +3,15 @@
 with open('day09_data.txt', 'r') as file:
     text = file.read().strip()
 
-text = text.split('\n')
+total = 0
+for line in text.split('\n'):
+    nums = [int(n) for n in line.split()]
+    final_nums = []
 
+    while set(nums) != {0}:
+        final_nums.append(nums[-1])
+        nums = [nums[i] - nums[i-1] for i in range(1, len(nums))]
 
-def f(xs):
-    """Process a list of integers."""
-    # Base case.
-    if all(x == 0 for x in xs):
-        return 0
+    total += sum(final_nums)
 
-    # Calculate the differences between consecutive elements in 'xs'.
-    differences = []
-    for i in range(len(xs) - 1):
-        differences.append(xs[i + 1] - xs[i])
-
-    # Recursively call 'f' with updated parameters.
-    return xs[-1] + 1 * f(differences)
-
-
-ans = 0
-
-for line in text:
-    # Convert each line into a list of integers
-    xs = [int(x) for x in line.split()]
-
-    # Add the result of 'f' to 'ans' after processing the list of integers 'xs'
-    ans += f(xs)
-
-print(ans)
+print(total)
--- AOC/aoc/day09b.py
+++ AOC/aoc/day09b.py
@@ -3,31 +3,16 @@
 with open('day09_data.txt', 'r') as file:
     text = file.read().strip()
 
-text = text.split('\n')
+total = 0
+for line in text.split('\n'):
+    nums = [int(n) for n in line.split()]
+    first_nums = []
 
+    while set(nums) != {0}:
+        first_nums.append(nums[0])
+        nums = [nums[i] - nums[i-1] for i in range(1, len(nums))]
 
-def f(xs):
-    """Process a list of integers."""
-    # Base case.
-    if all(x == 0 for x in xs):
-        return 0
+    for i, num in enumerate(first_nums):
+        total += num if i % 2 == 0 else -num
 
-    # Calculate the differences between consecutive elements in 'xs'.
-    differences = []
-    for i in range(len(xs) - 1):
-        differences.append(xs[i + 1] - xs[i])
-
-    # Recursively call 'f' with updated parameters.
-    return xs[0] + -1 * f(differences)
-
-
-ans = 0
-
-for line in text:
-    # Convert each line into a list of integers
-    xs = [int(x) for x in line.split()]
-
-    # Add the result of 'f' to 'ans' after processing the list of integers 'xs'
-    ans += f(xs)
-
-print(ans)
+print(total)
