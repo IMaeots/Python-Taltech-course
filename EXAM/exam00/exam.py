c867e9a0e3a57aa3703eff02815865eb55449b48
Indrek Mäeots <inmaeo@taltech.ee>
2024-01-02 13:36:53 +0200

    name to - in sorted

--- EXAM/exam00/exam.py
+++ EXAM/exam00/exam.py
@@ -270,7 +270,7 @@ def get_ordered_students(students: list) -> list:
 
     credit points (higher first), average_grade (higher first), name (a to z).
     """
-    return sorted(students, key=lambda x: (x.credit_points, x.average_grade, x.name), reverse=True)
+    return sorted(students, key=lambda x: (x.credit_points, x.average_grade, -x.name), reverse=True)
 
 
 class Room:
